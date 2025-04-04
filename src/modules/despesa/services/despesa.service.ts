6b02376d0f486f8d649ebfa4b8acf53ea3a82e45
Mateus Silva <dev.mateushssilva@gmail.com>
2025-04-04 08:49:28 -0300

    Altera nome repositório na service de despesa

--- src/modules/despesa/services/despesa.service.ts
+++ src/modules/despesa/services/despesa.service.ts
@@ -1,44 +1,42 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ServiceInterface } from 'src/interfaces/service_interface.interface';
 import { Repository } from 'typeorm';
 import { CreateDespesaDto } from '../dto/despesa/create_despesa.dto';
 import { UpdateDespesaDto } from '../dto/despesa/update_despesa.dto';
 import { Despesa } from '../entities/despesa.entity';
 
 @Injectable()
-export class DespesaService {
+export class DespesaService implements ServiceInterface<Despesa> {
   constructor(
     @InjectRepository(Despesa)
-    private despesasRepository: Repository<Despesa>,
+    private repository: Repository<Despesa>,
   ) {}
 
   async listar(): Promise<Despesa[]> {
-    return this.despesasRepository.find();
+    return this.repository.find();
   }
 
   async buscarPorId(id: number): Promise<Despesa | null> {
-    const despesa: Despesa | null = await this.despesasRepository.findOneBy({
+    const despesa: Despesa | null = await this.repository.findOneBy({
       id,
     });
 
     if (!despesa)
-      throw new HttpException(
-        'Despesa não encontrada!',
-        HttpStatus.BAD_REQUEST,
-      );
+      throw new HttpException('Despesa não encontrada!', HttpStatus.NOT_FOUND);
 
     return despesa;
   }
 
   async salvar(createDespesaDto: CreateDespesaDto): Promise<Despesa> {
-    return this.despesasRepository.save(createDespesaDto);
+    return this.repository.save(createDespesaDto);
   }
 
   async atualizar(
     id: number,
     updateDespesaDto: UpdateDespesaDto,
   ): Promise<Despesa> {
-    const despesa = await this.despesasRepository.findOneBy({ id });
+    const despesa = await this.repository.findOneBy({ id });
 
     if (!despesa)
       throw new HttpException(
@@ -48,11 +46,11 @@ export class DespesaService {
 
     Object.assign(despesa, updateDespesaDto);
 
-    return this.despesasRepository.save(despesa);
+    return this.repository.save(despesa);
   }
 
   async excluir(id: number): Promise<void> {
-    const despesa = await this.despesasRepository.findOneBy({ id });
+    const despesa = await this.repository.findOneBy({ id });
 
     if (!despesa)
       throw new HttpException(
@@ -60,7 +58,7 @@ export class DespesaService {
         HttpStatus.BAD_REQUEST,
       );
 
-    const result = await this.despesasRepository.delete({ id });
+    const result = await this.repository.delete({ id });
 
     if (!result.affected)
       throw new HttpException(
